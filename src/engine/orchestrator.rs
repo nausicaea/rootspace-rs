db6669764a58e0cbe775fa484fe7a48498118b0c
Eleanore Young <youngelliecaitlin@gmail.com>
2017-12-04 20:12:43 +0100

    Added a lot of documentation to Orchestrator.

--- src/engine/orchestrator.rs
+++ src/engine/orchestrator.rs
@@ -3,14 +3,20 @@ use std::time;
 
 use ecs::{World, EcsEvent, EventTrait};
 
+/// The `Orchestrator` owns the `World` and manages time (and the game loop).
 pub struct Orchestrator<E: EventTrait> {
+    /// Specifies the fixed time interval of the simulation.
     pub delta_time: time::Duration,
+    /// Specifies the maximum duration of a single frame.
     pub max_frame_time: time::Duration,
+    /// If `true`, activate debugging functionality.
     pub debug: bool,
+    /// Holds an instance of the `World`.
     pub world: World<E>,
 }
 
 impl<E: EventTrait> Orchestrator<E> {
+    /// Creates a new instance of the `Orchestrator`.
     pub fn new(debug: bool) -> Orchestrator<E> {
         Orchestrator {
             delta_time: time::Duration::from_millis(100),
@@ -19,11 +25,17 @@ impl<E: EventTrait> Orchestrator<E> {
             world: World::new(),
         }
     }
+    /// Initializes state and starts the game loop. Using the supplied closure, the state of the
+    /// `Orchestrator` and subsequently the `World` may be initialized.
     pub fn run<F>(&mut self, init: F) where F: FnOnce(&mut Orchestrator<E>) {
         init(self);
         self.world.dispatch(EcsEvent::Ready.into());
         self.main_loop();
     }
+    /// Runs the actual game loop. This loop uses a fixed time-step method to ensure that
+    /// `World::update` is called at a fixed interval, always. After a cycle of update calls,
+    /// the loop then issues calls to `World::render` and `World::handle_events`. The return value
+    /// of `World::handle_events` is used to terminate the loop.
     fn main_loop(&mut self) {
         let mut game_time = time::Duration::new(0, 0);
         let mut accumulator = time::Duration::new(0, 0);
