6a3d7cfcdcf16559eae9bc924b74d127feb0b18f
Eleanore Young <youngelliecaitlin@gmail.com>
2017-12-04 18:25:35 +0100

    Added a logging system (log and fern).

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
 #![feature(core_intrinsics)]
 
 extern crate clap;
+#[macro_use]
+extern crate log;
+extern crate fern;
 
 mod ecs;
 mod game;
 
+use std::io;
 use clap::{Arg, App};
+use log::LogLevelFilter;
+use fern::Dispatch;
 use game::Orchestrator;
 
 fn main() {
+    // Define the command line interface.
     let matches = App::new(env!("CARGO_PKG_NAME"))
         .version(env!("CARGO_PKG_VERSION"))
         .author(env!("CARGO_PKG_AUTHORS"))
@@ -22,11 +29,45 @@ fn main() {
              .long("verbose")
              .multiple(true)
              .help("Determines the amount of output logged"))
+        .arg(Arg::with_name("quiet")
+             .short("q")
+             .long("quiet")
+             .conflicts_with("verbosity")
+             .help("Disables all output"))
         .get_matches();
 
+    // Obtain the command line arguments.
     let debugging = matches.is_present("debugging");
     let verbosity = matches.occurrences_of("verbosity");
+    let quiet = matches.is_present("quiet");
 
+    // Configure the logging system.
+    let log_level_filter = if quiet {
+        LogLevelFilter::Off
+    } else {
+        match verbosity {
+            0 => LogLevelFilter::Error,
+            1 => LogLevelFilter::Warn,
+            2 => LogLevelFilter::Info,
+            3 => LogLevelFilter::Debug,
+            _ => LogLevelFilter::Trace,
+        }
+    };
+    Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{} @{}: {}",
+                record.level(),
+                record.target(),
+                message
+            ))
+        })
+        .level(log_level_filter)
+        .chain(io::stdout())
+        .apply()
+        .unwrap();
+
+    // Create the engine instance and run it.
     let mut orchestrator = Orchestrator::new(debugging);
     orchestrator.run();
 }
