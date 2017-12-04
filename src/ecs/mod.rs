bd20f57f39348763dd9ffb1328f17d82cf194e41
Eleanore Young <youngelliecaitlin@gmail.com>
2017-12-04 20:18:31 +0100

    Added module-level documentation for the ecs.

--- src/ecs/mod.rs
+++ src/ecs/mod.rs
@@ -1,5 +1,13 @@
 #![allow(dead_code)]
 
+//! The `ecs` module provides functionality to represent an Entity-Component-System architecture.
+//! The `World` collects multiple boxed `SystemTrait`s which operate on the `ComponentTrait`s of
+//! the registered `Entities` (in an `Assembly`) via three main loop stages: event handling,
+//! simulation updates, and state rendering. Systems (types that implement `SystemTrait`) may be
+//! added dynamically to `World`, as well as components (types that implement `ComponentTrait`. The
+//! latter must be linked to an `Entity` however, which must first be created by the `Assembly` (or
+//! the `World` via Deref trait).
+
 /// Returns the names of multiple types as String.
 macro_rules! type_names {
     ($t:tt) => {{
