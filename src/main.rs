5a267c4da4dec85405b96df851d9ba6d5b0f1f53
Eleanore Young <youngelliecaitlin@gmail.com>
2017-12-04 11:21:01 +0100

    Added clap as a dependency an wrote a basic command line interface.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,32 @@
 #![feature(core_intrinsics)]
 
+extern crate clap;
+
 mod ecs;
 mod game;
 
+use clap::{Arg, App};
 use game::Orchestrator;
 
 fn main() {
-    let debug = true;
+    let matches = App::new("Rootspace")
+        .version("0.1.0")
+        .author("Eleanore Young")
+        .about("A game wrapped in uncertainty and mis(t)ery")
+        .arg(Arg::with_name("debugging")
+             .short("d")
+             .long("debug")
+             .help("Enables debugging features"))
+        .arg(Arg::with_name("verbosity")
+             .short("v")
+             .long("verbose")
+             .multiple(true)
+             .help("Determines the amount of output logged"))
+        .get_matches();
 
-    let mut orchestrator = Orchestrator::new(debug);
+    let debugging = matches.is_present("debugging");
+    let verbosity = matches.occurrences_of("verbosity");
+
+    let mut orchestrator = Orchestrator::new(debugging);
     orchestrator.run();
 }
