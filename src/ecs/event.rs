bab67b1b1aea07f70505ab90b50c727df2281a67
Eleanore Young <youngelliecaitlin@gmail.com>
2017-12-08 06:58:47 +0100

    Removed the EventTrait inheritance of Into<EventFilter>.

--- src/ecs/event.rs
+++ src/ecs/event.rs
@@ -16,7 +16,7 @@ pub const ALL_EVENTS: EventFlag = u64::MAX;
 
 /// Every engine event must implement the trait `EventTrait`, as some events must be converted to
 /// `EcsEvent`s for `World` to interact properly with the engine.
-pub trait EventTrait: fmt::Debug + Clone + Into<EventFlag> + From<EcsEvent> {
+pub trait EventTrait: fmt::Debug + Clone + From<EcsEvent> {
     /// Returns true if the specified filter selects the current enum variant.
     fn match_filter(&self, filter: EventFlag) -> bool;
     /// Attempts to convert to a `EcsEvent`.
