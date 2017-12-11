cd7ef2398933fb7be87275066d2dbce42c29bfdd
Eleanore Young <youngelliecaitlin@gmail.com>
2017-12-11 09:21:25 +0100

    Added the uniform pvm_matrix.

--- src/engine/graphics/uniforms.rs
+++ src/engine/graphics/uniforms.rs
@@ -1,10 +1,15 @@
+use nalgebra::Matrix4;
 use glium::uniforms;
+use super::super::Float;
 
 pub struct Uniforms {
+    pvm_matrix: Matrix4<Float>,
 }
 
 impl uniforms::Uniforms for Uniforms {
-    fn visit_values<'a, F>(&'a self, f: F) where F: FnMut(&str, uniforms::UniformValue<'a>) {
+    fn visit_values<'a, F>(&'a self, mut f: F) where F: FnMut(&str, uniforms::UniformValue<'a>) {
+        f("pvm_matrix", uniforms::UniformValue::Mat4(self.pvm_matrix.into()));
         // f("name", uniform_value);
+        // I need: pvm_matrix, normal_matrix, optionally diff_tex, optionally norm_tex
     }
 }
