d1fa8f354274bbf1edfb3aaa3900db8ebb820ab2
Eleanore Young <youngelliecaitlin@gmail.com>
2017-12-16 17:34:05 +0100

    Added comments and deleted superfluous variables.

--- engine/src/interface/user_interface.rs
+++ engine/src/interface/user_interface.rs
@@ -76,14 +76,19 @@ impl UserInterface {
         }
     }
     fn create_speech_bubble(&self, entities: &mut Assembly, target: &str, content: &str, lifetime: u64) -> Result<(), UiError> {
+        // Attempt to find the entity named in `target` and retreive its world position.
         let entity_pos_world = entities.rsf2::<_, Description, Model>(|&(d, _)| d.name == target)
             .map(|(_, m)| Point3::from_coordinates(m.translation.vector))?;
 
+        // Project the entity position to normalized device coordinates (this requires the camera
+        // entity).
         let entity_pos_ndc = entities.rs2::<Projection, View>()
             .map(|(p, v)| p.project_point(&v.transform_point(&entity_pos_world)))?;
 
+        // Obtain a mutable reference to the `UiState`.
         let ui_state = entities.ws1::<UiState>()?;
 
+        // Layout the glyphs based on the textual `content`.
         let (glyphs, text_dims_px) = layout_paragraph_cached(&mut ui_state.font_cache_cpu,
                                                              &ui_state.font_cache_gpu,
                                                              &ui_state.common.font,
@@ -91,27 +96,27 @@ impl UserInterface {
                                                              ui_state.speech_bubble.width,
                                                              content)?;
 
-        // Calculate positions and dimensions of the involved primitives.
+        // Calculate positions and dimensions of the involved primitives: Text and Rect.
         let dimensions = Vector2::new(ui_state.dimensions[0] as f32, ui_state.dimensions[1] as f32);
-
         let margin_left = ui_state.speech_bubble.margin_left as f32 / dimensions.x;
         let margin_right = ui_state.speech_bubble.margin_right as f32 / dimensions.x;
         let margin_top = ui_state.speech_bubble.margin_top as f32 / dimensions.y;
         let margin_bottom = ui_state.speech_bubble.margin_bottom as f32 / dimensions.y;
         let relative_offset = ui_state.speech_bubble.relative_position_offset;
 
-        let text_dims_px = Vector2::new(text_dims_px[0] as f32, text_dims_px[1] as f32);
-        let text_dims_ndc = text_dims_px.component_div(&dimensions);
+        let text_dims_ndc = Vector2::new(text_dims_px[0] as f32, text_dims_px[1] as f32)
+            .component_div(&dimensions);
         let rect_dims_ndc = text_dims_ndc + Vector2::new(margin_left + margin_right, margin_top + margin_bottom);
 
-        let entity_pos_ndc = Vector2::new(entity_pos_ndc.x, entity_pos_ndc.y);
-        let bubble_center = entity_pos_ndc + relative_offset.component_mul(&rect_dims_ndc);
+        let element_center = Vector2::new(entity_pos_ndc.x, entity_pos_ndc.y) + relative_offset.component_mul(&rect_dims_ndc);
         let text_center = Vector2::new(text_dims_ndc.x - rect_dims_ndc.x, rect_dims_ndc.y - text_dims_ndc.y) / 2.0 + Vector2::new(margin_left, -margin_top);
 
-        let element_model = Model::new(&Vector3::new(bubble_center.x, bubble_center.y, -0.98), &nalgebra::zero());
+        // Create the model matrices from the above values.
+        let element_model = Model::new(&Vector3::new(element_center.x, element_center.y, -0.98), &nalgebra::zero());
         let text_model = Model::new(&Vector3::new(text_center.x, text_center.y, -0.01), &nalgebra::zero());
         let rect_model = Model::new(&nalgebra::zero(), &nalgebra::zero());
 
+        // Create the primitive materials.
         let text_material = Material::new(&self.display,
                                           &ui_state.speech_bubble.text_vertex_shader,
                                           &ui_state.speech_bubble.text_fragment_shader, None, None,
@@ -123,11 +128,13 @@ impl UserInterface {
                                           Some(&ui_state.speech_bubble.rect_diffuse_texture),
                                           None)?;
 
+        // Create the primitives.
         let rect = UiPrimitive::new_rect(&self.display, &rect_dims_ndc.into(), 0.0, rect_model, rect_material)?;
         let text = UiPrimitive::new_text(&self.display, &ui_state.dimensions, 0.0,
                                          &ui_state.font_cache_cpu, &glyphs, &text_dims_ndc.into(),
                                          text_model, text_material)?;
 
+        // Create and register the element.
         let id = Uuid::new_v4();
         ui_state.elements.insert(id, UiElement::new(element_model, vec![rect, text]));
         ui_state.lifetimes.insert(id, (Instant::now(), Duration::new(lifetime, 0)));
