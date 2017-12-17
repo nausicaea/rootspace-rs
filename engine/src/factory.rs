950263a3fc7963a27b36c028469a53e08a749c7e
Eleanore Young <youngelliecaitlin@gmail.com>
2017-12-17 15:09:03 +0100

    Implemented caching for Materials.

--- engine/src/factory.rs
+++ engine/src/factory.rs
@@ -1,80 +1,50 @@
-use std::io;
-use std::path::{Path, PathBuf};
-use std::rc::Rc;
-use glium::{Display, Program, Texture2d};
-use glium::{program, texture};
-use image;
-use utilities::{load_text_file, load_image_file};
+use std::collections::hash_map::{HashMap, DefaultHasher};
+use std::hash::{Hash, Hasher};
+use std::path::Path;
+use glium::Display;
+use components::material::{Material, MaterialError};
 
 #[derive(Debug, Fail)]
 pub enum FactoryError {
     #[fail(display = "{}", _0)]
-    ShaderError(#[cause] program::ProgramCreationError),
-    #[fail(display = "{}", _0)]
-    IoError(#[cause] io::Error),
-    #[fail(display = "{}", _0)]
-    ImageError(#[cause] image::ImageError),
-    #[fail(display = "{}", _0)]
-    TextureError(#[cause] texture::TextureCreationError),
+    MaterialCreationError(#[cause] MaterialError),
 }
 
-impl From<program::ProgramCreationError> for FactoryError {
-    fn from(value: program::ProgramCreationError) -> Self {
-        FactoryError::ShaderError(value)
+impl From<MaterialError> for FactoryError {
+    fn from(value: MaterialError) -> Self {
+        FactoryError::MaterialCreationError(value)
     }
 }
 
-impl From<io::Error> for FactoryError {
-    fn from(value: io::Error) -> Self {
-        FactoryError::IoError(value)
-    }
-}
-
-impl From<image::ImageError> for FactoryError {
-    fn from(value: image::ImageError) -> Self {
-        FactoryError::ImageError(value)
-    }
-}
-
-impl From<texture::TextureCreationError> for FactoryError {
-    fn from(value: texture::TextureCreationError) -> Self {
-        FactoryError::TextureError(value)
-    }
-}
-
+/// `ComponentFactory` provides a way to create components and cache them for reuse later. Due to
+/// the internal use of `Rc`, componens created thus are immutable.
+#[derive(Default)]
 pub struct ComponentFactory {
-    resource_path: PathBuf,
+    materials: HashMap<u64, Material>,
 }
 
 impl ComponentFactory {
-    pub fn new(resource_path: &Path) -> Self {
-        ComponentFactory {
-            resource_path: resource_path.to_owned(),
+    pub fn new() -> Self {
+        Default::default()
+    }
+    pub fn new_material(&mut self, display: &Display, vs: &Path, fs: &Path, gs: Option<&Path>, dt: Option<&Path>, nt: Option<&Path>) -> Result<Material, FactoryError> {
+        let hash = self.calculate_material_hash(vs, fs, gs, dt, nt);
+
+        if self.materials.contains_key(&hash) {
+            Ok(self.materials.get(&hash).unwrap_or_else(|| unreachable!()).clone())
+        } else {
+            let material = Material::new(display, vs, fs, gs, dt, nt)?;
+            self.materials.insert(hash, material.clone());
+            Ok(material)
         }
     }
-    pub fn new_material(&mut self, display: &Display, vs: &Path, fs: &Path, gs: Option<&Path>, dt: Option<&Path>, nt: Option<&Path>) -> Result<(), FactoryError> {
-        let vss = load_text_file(vs)?;
-        let fss = load_text_file(fs)?;
-        let gss = match gs {
-            Some(gp) => Some(load_text_file(gp)?),
-            None => None,
-        };
-        let shader = Rc::new(Program::from_source(display, &vss, &fss, gss.as_ref().map(|g| &**g))?);
-        let dt = match dt {
-            Some(dp) => {
-                let di = load_image_file(dp)?;
-                Some(Rc::new(Texture2d::new(display, di)?))
-            },
-            None => None,
-        };
-        let nt = match nt {
-            Some(np) => {
-                let ni = load_image_file(np)?;
-                Some(Rc::new(Texture2d::new(display, ni)?))
-            },
-            None => None,
-        };
-
-        Ok(())
+    fn calculate_material_hash(&self, vs: &Path, fs: &Path, gs: Option<&Path>, dt: Option<&Path>, nt: Option<&Path>) -> u64 {
+        let mut s = DefaultHasher::new();
+        vs.hash(&mut s);
+        fs.hash(&mut s);
+        gs.hash(&mut s);
+        dt.hash(&mut s);
+        nt.hash(&mut s);
+        s.finish()
     }
 }
