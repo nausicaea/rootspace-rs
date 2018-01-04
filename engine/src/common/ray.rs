e811fca00c4a80aae36fcddc36b97a86f209d50a
Eleanore Young <youngelliecaitlin@gmail.com>
2018-01-04 10:33:21 +0100

    Added Clone and Debug to Ray, as well as documentation.

--- engine/src/common/ray.rs
+++ engine/src/common/ray.rs
@@ -1,8 +1,12 @@
 use nalgebra::{Point3, Vector3, Scalar};
 
-/// Characterises a ray (a line segment with an origin and direction).
+/// A `Ray` characterises a ray (a line segment with an origin, direction and infinite length in
+/// that direction).
+#[derive(Debug, Clone)]
 pub struct Ray<N> where N: Scalar {
+    /// Specifies the origin of the `Ray`.
     pub origin: Point3<N>,
+    /// Specifies the direction of the `Ray`.
     pub direction: Vector3<N>,
 }
 
