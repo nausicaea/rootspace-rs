e3a2a1032aec4f9be6093c87f25e920f544fb093
Eleanore Young <youngelliecaitlin@gmail.com>
2018-01-04 09:41:34 +0100

    Ray is now generic wrt the scalar parameter.

--- engine/src/common/ray.rs
+++ engine/src/common/ray.rs
@@ -1,8 +1,8 @@
-use nalgebra::{Point3, Vector3};
+use nalgebra::{Point3, Vector3, Scalar};
 
 /// Characterises a ray (a line segment with an origin and direction).
-pub struct Ray {
-    pub origin: Point3<f32>,
-    pub direction: Vector3<f32>,
+pub struct Ray<N> where N: Scalar {
+    pub origin: Point3<N>,
+    pub direction: Vector3<N>,
 }
 
