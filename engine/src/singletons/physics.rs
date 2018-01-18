611512430818978b791a0c80809ae2612cea5d5d
Eleanore Young <youngelliecaitlin@gmail.com>
2018-01-18 14:11:47 +0100

    Updated the documentation in physics.

--- engine/src/singletons/physics.rs
+++ engine/src/singletons/physics.rs
@@ -4,13 +4,15 @@ use common::ray::{Ray, RaycastHit};
 use components::model::Model;
 use components::bounding_volume::BoundingVolume;
 
+/// The `PhysicsController` provides means to perform collision detection and other physics
+/// operations.
 #[derive(Default)]
 pub struct PhysicsController {}
 
 impl PhysicsController {
     /// Returns the first intersection of the ray with any object in the `Assembly` with a
-    /// `BoundingVolume` component. Currently no spatial partitioning is performed, thus, this
-    /// algorithm is likely to be very slow.
+    /// `BoundingVolume` component. Currently no spatial partitioning or caching is performed,
+    /// thus, this algorithm is likely to be very slow.
     pub fn raycast(&mut self, entities: &Assembly, ray: &Ray<f32>) -> Option<RaycastHit> {
         for (e, m, b) in entities.r2::<Model, BoundingVolume>() {
             // Decompose the model matrix into a TRS matrix. TODO: This call should be cached somehow.
