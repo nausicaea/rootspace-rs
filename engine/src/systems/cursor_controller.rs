6fa295075bbfd3f0a694c288b1617a6b9677dce1
Eleanore Young <youngelliecaitlin@gmail.com>
2018-01-29 09:49:21 +0100

    Added some comments to the code in CursorController.

--- engine/src/systems/cursor_controller.rs
+++ engine/src/systems/cursor_controller.rs
@@ -26,26 +26,54 @@ impl<A> SystemTrait<EngineEvent, A> for CursorController {
         let mut resulting_event = None;
 
         match *event {
-            EngineEvent::CursorPosition(p) => {
-                // Update the cursor component.
+            EngineEvent::CursorPosition(position) => {
+                // Update the cursor component's position.
                 entities.ws1::<Cursor>()
-                    .map(|(_, c)| c.position = p)
+                    .map(|(_, c)| c.position = position)
                     .expect("Could not access the Cursor component");
             },
-            EngineEvent::MouseInput(b, s) => {
-                match b {
+            EngineEvent::MouseInput(button, state) => {
+                // Given the current button state and the new one, emit an event if the button went
+                // from released to pressed (eg. a down flank) or from pressed to released (eg. an
+                // up flank). Subsequently update the button state on the cursor component.
+                match button {
                     MouseButton::Left => {
-                        entities.ws1::<Cursor>()
+                        entities.rs1::<Cursor>()
                             .map(|(_, c)| {
                                 match c.left_button {
-                                    ElementState::Pressed => match s {
+                                    ElementState::Pressed => match state {
                                         ElementState::Pressed => (),
-                                        ElementState::Released =>
+                                        ElementState::Released => trace!("Left mouse input up flank received"),
+                                    },
+                                    ElementState::Released => match state {
+                                        ElementState::Pressed => trace!("Left mouse input down flank received"),
+                                        ElementState::Released => (),
+                                    },
                                 }
                             })
                             .expect("Could not access the Cursor component");
+                        entities.ws1::<Cursor>()
+                            .map(|(_, c)| c.left_button = state)
+                            .expect("Could not access the Cursor component");
                     },
                     MouseButton::Right => {
+                        entities.rs1::<Cursor>()
+                            .map(|(_, c)| {
+                                match c.right_button {
+                                    ElementState::Pressed => match state {
+                                        ElementState::Pressed => (),
+                                        ElementState::Released => trace!("Right mouse input up flank received"),
+                                    },
+                                    ElementState::Released => match state {
+                                        ElementState::Pressed => trace!("Right mouse input down flank received"),
+                                        ElementState::Released => (),
+                                    },
+                                }
+                            })
+                            .expect("Could not access the Cursor component");
+                        entities.ws1::<Cursor>()
+                            .map(|(_, c)| c.right_button = state)
+                            .expect("Could not access the Cursor component");
                     },
                     _ => (),
                 }
